69d57a7f75927a2a6e897784706dcb6670bb14ce
Stavros Korokithakis <hi@stavros.io>
2025-11-05 19:09:20 +0200

    Admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.html import format_html
 from djangoql.admin import DjangoQLSearchMixin
 
 from .models import Article
@@ -12,16 +11,6 @@ admin.site.register(User, UserAdmin)
 @admin.register(Article)
 class ArticleAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     list_display = ["title", "updated_at"]
-    readonly_fields = ["slug", "created_at", "updated_at", "view_article_link"]
+    readonly_fields = ["slug", "created_at", "updated_at"]
     search_fields = ["title", "content", "summary_snippet"]
     ordering = ["title"]
-
-    @admin.display(description="Article Link")
-    def view_article_link(self, obj: Article) -> str:
-        """Display a link to view the article on the site."""
-        if obj.pk:
-            url = obj.get_absolute_url()
-            return format_html(
-                '<a href="{}" target="_blank">View Article on Site</a>', url
-            )
-        return "-"
